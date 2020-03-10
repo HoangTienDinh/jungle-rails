680035882dfdd8ff2a3fbe5174b76f66b374db3d
HoHoHoang <hoangdinh90@gmail.com>
2020-03-10 00:42:09 +0000

    add user_spec for testing, added validations for models/user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 3 }
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.email.downcase!
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new({name: 'Hoho', email: 'hoho@mail.com', password: '1234'})
+  }
+
+  describe 'Validations' do
+    it 'Has a valid name' do
+      expect(subject).to be_valid
+    end
+
+    it 'Has a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+    
+  end
 end
